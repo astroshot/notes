76adcedfb0ae5a8a1656326b770d74c1b314e338
liuzhao11 <liuzhao11@baidu.com>
2019-04-04 16:07:59 +0800

    compile vim 8.1 on centos 6

--- config/vim/compile.sh
+++ config/vim/compile.sh
@@ -14,3 +14,17 @@
   --with-python3-config-dir=$HOME/.local/anaconda3/lib/python3.6/config \
   --with-python-config-dir=/usr/lib/python2.6/config \
   --without-x
+
+
+# compile using anaconda3
+./configure --enable-terminal --enable-gui=auto \
+      --with-features=huge \
+      --enable-multibyte \
+       --enable-cscope --enable-terminal --enable-gui=auto  \
+      --prefix=$HOME/.local/vim \
+   --enable-python3interp \
+   --with-python3-config-dir=$HOME/.local/anaconda3/lib/python3.6/config \
+   --includedir=$HOME/.local/anaconda3/include  \
+   LDFLAGS=" -L$HOME/.local/anaconda3/lib -L$HOME/.local/anaconda3/lib/python3.6/config-3.6m-x86_64-linux-gnu"
+
+# then export LD_LIBRARY_PATH="$HOME/.local/anaconda3/lib:$HOME/.local/lib:$LD_LIBRARY_PATH"
