a5f9bab9aeb1e616ee0761b9a9585eda1cbde86e
liuzhao11 <liuzhao11@baidu.com>
2019-05-15 09:15:55 +0800

    update zshrc to p10k

--- config/zsh/get_zsh_plugins.sh
+++ config/zsh/get_zsh_plugins.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-echo 'Downloading powerlevel9k...'
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# echo 'Downloading powerlevel9k...'
+# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+echo 'Getting powerlevel10k'
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 
 echo 'Downloading zsh-highlighting...'
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
