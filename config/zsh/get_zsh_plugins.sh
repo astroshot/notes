4c6f79d9eab2e9569816153613bfa0b7acb45a21
Shockwave <astroshot@outlook.com>
2019-12-11 17:19:29 +0800

    fix zsh theme install directory

--- config/zsh/get_zsh_plugins.sh
+++ config/zsh/get_zsh_plugins.sh
@@ -4,8 +4,7 @@
 # git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 echo 'Getting powerlevel10k'
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
+git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/powerlevel10k
 
 echo 'Downloading zsh-highlighting...'
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
